e19dba76588533432537826dc3b0e8e19186845a
Randy J. Ray <rjray@blackperl.com>
2022-08-14 12:02:31 -0700

    Checkpoint: basic stuff working

--- src/lib.rs
+++ src/lib.rs
@@ -5,101 +5,182 @@ use opts::Cli;
 use std::{
     error::Error,
     fs::{self, Metadata},
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 
+// Simplify cases where an error can be returned, but no specific "normal"
+// return value is needed.
 type SimpleResult<T> = Result<T, Box<dyn Error>>;
 
+#[derive(Debug)]
 struct Entry {
-    path: PathBuf,
+    name: String,
     meta: Metadata,
 }
 
-struct PendingDir {
+#[derive(Debug)]
+struct Directory {
     path: PathBuf,
+    name: String,
     meta: Metadata,
 }
 
 pub fn run() -> SimpleResult<()> {
-    let config = parse_args();
+    // Get the configuration based on the command-line arguments:
+    let config = Cli::parse();
+
+    // `paths` is the list of command-line-provided paths to list. It is
+    // cloned here so that we aren't borrowing from `config` when we process
+    // the list.
+    let paths = if config.paths.is_empty() {
+        vec![String::from(".")]
+    } else {
+        config.paths.clone()
+    };
+
+    // These will hold the entries and directories for the initial reading of
+    // the paths and such passed on the command-line.
+    let mut entries: Vec<Entry> = Vec::new();
+    let mut dirs: Vec<Directory> = Vec::new();
+
+    if paths.len() == 1 {
+        // When there is only one path in the list, it is handled a little
+        // differently.
+        get_initial_entries(&paths[0], true, &mut entries, &mut dirs, &config)?;
+    } else {
+        // More than one path specified means that we do at least one level of
+        // recursion into (top-level) directories, unless `-d` was passed.
+        for path in paths {
+            get_initial_entries(
+                &path,
+                false,
+                &mut entries,
+                &mut dirs,
+                &config,
+            )?;
+        }
+    }
+
+    produce_output(&mut entries, &mut dirs, &config)?;
 
     Ok(())
 }
 
-fn parse_args() -> Cli {
-    let cli = Cli::parse();
-    println!("all: {:?}", cli.all);
-    println!("recursive: {:?}", cli.recursive);
-    println!("paths: {:?}", cli.paths);
+fn get_initial_entries(
+    path: &String,
+    solo: bool,
+    entries: &mut Vec<Entry>,
+    dirs: &mut Vec<Directory>,
+    config: &Cli,
+) -> SimpleResult<()> {
+    match fs::metadata(path) {
+        Err(e) => eprintln!("{}: {}", path, e),
+        Ok(meta) => {
+            if meta.is_dir() {
+                // When this is a directory, we may or may not explore its
+                // contents, depending on whether it is the only path, etc.
+                if solo && !config.recursive {
+                    // Process the content of the directory directly and
+                    // schedule it for output. If config.recursive was true
+                    // then we don't do this, we just queue up the dir for
+                    // regular processing.
+                    process_directory(path, entries, dirs, config)?;
+                } else {
+                    // If config.directory is not true, then we add it to
+                    // the list of directories so it can be recursively
+                    // read. Otherwise, it becomes an entry.
+                    if !config.directory {
+                        dirs.push(Directory {
+                            path: PathBuf::from(path),
+                            name: String::from(path),
+                            meta,
+                        });
+                    } else {
+                        entries.push(Entry { name: String::from(path), meta })
+                    }
+                }
+            } else {
+                entries.push(Entry { name: String::from(path), meta })
+            }
+        }
+    }
 
-    cli
+    Ok(())
 }
 
-// fn get_file_entries(paths_in: Vec<String>, config: Config) -> SimpleResult<Vec<Entry>> {
-//     let mut results: Vec<Entry> = Vec::new();
-//     let mut pending: Vec<PendingDir> = Vec::new();
-//     let mut paths: Vec<String> = Vec::new();
-//     paths.extend(paths_in);
-//     if paths.len() == 0 {
-//         paths.push(String::from("."));
-//     }
-//     let args_length = paths.len();
+fn process_directory(
+    name: &String,
+    entries: &mut Vec<Entry>,
+    dirs: &mut Vec<Directory>,
+    config: &Cli,
+) -> SimpleResult<()> {
+    if config.all {
+        // `fs::read_dir()` does not include the . and .. entries from a
+        // directory. This is... unfortunate for actual systems programming.
+        let base_path = Path::new(name);
+        let dot = base_path.join(".");
+        let dot_dot = base_path.join("..");
+        entries.push(Entry { name: ".".to_string(), meta: dot.metadata()? });
+        entries
+            .push(Entry { name: "..".to_string(), meta: dot_dot.metadata()? });
+    }
+    // Proceed to handle all the entries that can be read from this directory
+    for entry in fs::read_dir(name)? {
+        let entry = entry?;
+        let path = entry.path();
+        // This is modified from the chapter 14 code from "Command-Line Rust",
+        // and gets the leaf file-name from `path` while also down-converting
+        // it to a string.
+        let name = path.file_name().map_or(String::from(""), |fname| {
+            fname.to_string_lossy().to_string()
+        });
+        let hidden = name.starts_with('.');
+        if !hidden || config.all {
+            let meta = entry.metadata()?;
+            if meta.is_dir() && config.recursive {
+                // Only register this as a directory if -R was passed
+                dirs.push(Directory { path, meta, name });
+            } else {
+                entries.push(Entry { meta, name });
+            }
+        }
+    }
+    Ok(())
+}
 
-//     for name in paths {
-//         match fs::metadata(&name) {
-//             Err(e) => eprintln!("{}: {}", name, e),
-//             Ok(meta) => {
-//                 if meta.is_dir() {
-//                     // If there is just the one directory in our list of paths
-//                     // then process it immediately. Otherwise add it to the
-//                     // pending list.
-//                     if args_length == 1 {
-//                         process_directory(&config, &name, &mut results, &mut pending)?;
-//                     } else {
-//                         pending.push(PendingDir {
-//                             path: PathBuf::from(name),
-//                             meta,
-//                         });
-//                     }
-//                 } else {
-//                     results.push(Entry {
-//                         path: PathBuf::from(name),
-//                         meta,
-//                     })
-//                 }
-//             }
-//         }
-//     }
+fn produce_output(
+    entries: &mut Vec<Entry>,
+    dirs: &mut Vec<Directory>,
+    config: &Cli,
+) -> SimpleResult<()> {
+    // Used in tracking whether we need to emit a newline before a given
+    // directory name when displaying a sub-dir's content.
+    let mut need_newline = !entries.is_empty();
 
-//     Ok(results)
-// }
+    // Do the entries of this directory.
+    entries.sort_by(|a, b| a.name.cmp(&b.name));
+    for entry in entries {
+        println!("{}", entry.name);
+    }
 
-// fn process_directory(
-//     config: &Config,
-//     name: &String,
-//     results: &mut Vec<Entry>,
-//     pending: &mut Vec<PendingDir>,
-// ) -> SimpleResult<()> {
-//     for entry in fs::read_dir(name)? {
-//         let entry = entry?;
-//         let path = entry.path();
-//         let hidden = path.file_name().map_or(false, |file_name| {
-//             file_name.to_string_lossy().starts_with('.')
-//         });
-//         if !hidden || config.show_hidden {
-//             let meta = fs::metadata(name)?;
-//             if meta.is_dir() && config.recurse {
-//                 pending.push(PendingDir {
-//                     path: PathBuf::from(path),
-//                     meta,
-//                 });
-//             } else {
-//                 results.push(Entry {
-//                     path: PathBuf::from(path),
-//                     meta,
-//                 });
-//             }
-//         }
-//     }
-//     Ok(())
-// }
+    // Process the sub-dirs of this directory. This will only be done when
+    // either a directory is a command-line path or -R is given.
+    dirs.sort_by(|a, b| a.name.cmp(&b.name));
+    for dir in dirs {
+        let mut entries: Vec<Entry> = Vec::new();
+        let mut dirs: Vec<Directory> = Vec::new();
+        let dir_name = String::from(dir.path.to_string_lossy());
+
+        if need_newline {
+            println!();
+        }
+        need_newline |= true;
+        println!("{}:", &dir_name);
+
+        process_directory(&dir_name, &mut entries, &mut dirs, config)?;
+
+        produce_output(&mut entries, &mut dirs, config)?;
+    }
+
+    Ok(())
+}
--- src/opts.rs
+++ src/opts.rs
@@ -1,15 +1,29 @@
 use clap::ArgAction::Append;
-use clap::Parser;
+use clap::{ArgGroup, Parser};
 
 #[derive(Parser)]
 #[clap(author, version, about)]
+#[clap(group(
+    ArgGroup::new("dots").args(&["all", "almost-all"])
+))]
 pub struct Cli {
+    /// Optional specific paths to list
     #[clap(value_parser, action = Append)]
     pub paths: Vec<String>,
 
+    /// Do not ignore entries whose names start with .
     #[clap(short, long)]
     pub all: bool,
 
+    /// Do not list the implied . and .. directory entries
+    #[clap(short = 'A', long)]
+    pub almost_all: bool,
+
+    /// List directories themselves, not their contents
+    #[clap(short, long)]
+    pub directory: bool,
+
+    /// List subdirectories recursively
     #[clap(short = 'R', long)]
     pub recursive: bool,
 }
