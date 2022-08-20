529d5574395c3de5c27c0a11b9fad8143fd3d5c0
Randy J. Ray <rjray@blackperl.com>
2022-08-19 21:16:34 -0700

    Various work, checkpoint

--- src/output.rs
+++ src/output.rs
@@ -14,36 +14,44 @@ struct OutputName {
 }
 
 /*
-   Output the set of names passed in. Assume they are already sorted by the
-   order that the configuration specified.
+   Output the set of names passed in, simple columns with names only. Assume
+   they are already sorted by the order that the configuration specified.
 */
-pub fn output_names(entries: &Vec<Entry>, width: u32, config: &Cli) {
+pub fn output_names_simple(entries: &Vec<Entry>, width: u32, config: &Cli) {
+    let count: usize = entries.len();
     let names: Vec<OutputName> = entries
-        .into_iter()
+        .iter()
         .map(|e| entry_to_output_with_frills(e, config))
         .collect();
     let max_len: usize = names.iter().map(|e| e.size).max().unwrap();
-    let quoted: bool = names.iter().any(|e| e.quoted);
+    let any_quoted: bool = names.iter().any(|e| e.quoted);
+    let all_quoted: bool = config.quote_name;
     let mut leader: u8 = 0;
     let mut spacer: u8 = 2;
-    if quoted {
+    if any_quoted && !all_quoted {
         leader += 1;
         spacer += 1;
     }
     let mut cols: u8 = 1;
-    loop {
-        let w = max_len as u8 * cols + spacer * (cols - 1) + leader;
-        if w as u32 > width {
-            break;
-        } else {
-            cols += 1;
+    if !(config.one || config.zero) {
+        loop {
+            let w = max_len as u8 * cols + spacer * (cols - 1) + leader;
+            if w as u32 > width {
+                break;
+            } else {
+                cols += 1;
+            }
         }
     }
+    // Determine the full-length column size and the remainder (if any) size.
 }
 
 fn entry_to_output_with_frills(entry: &Entry, config: &Cli) -> OutputName {
     lazy_static! {
         static ref WS: Regex = Regex::new(r"(\s)").unwrap();
+    }
+    lazy_static! {
+        static ref SP: Regex = Regex::new(r"[/*@=|]").unwrap();
     }
     lazy_static! {
         static ref SQ: Regex = Regex::new(r"(')").unwrap();
@@ -53,14 +61,16 @@ fn entry_to_output_with_frills(entry: &Entry, config: &Cli) -> OutputName {
     }
 
     let base_name: String = entry.name.clone();
-    let needs_quote: bool = match base_name.find(char::is_whitespace) {
+    let has_whitespace: bool = match base_name.find(char::is_whitespace) {
         Some(_) => true,
         None => false,
     };
+    let has_special: bool = SP.is_match(&base_name);
+    let needs_quote = has_whitespace || has_special;
+
     let quoted_name: String = if config.quote_name {
         // The CLI options require all names be quoted. Use "" for quoting.
-        let mut quoted = WS.replace_all(&base_name, "\\$1").into_owned();
-        quoted = DQ.replace_all(&quoted, "\\$1").into_owned();
+        let mut quoted = DQ.replace_all(&base_name, "\\$1").into_owned();
         quoted.insert(0, '"');
         quoted.push('"');
         quoted
@@ -78,6 +88,6 @@ fn entry_to_output_with_frills(entry: &Entry, config: &Cli) -> OutputName {
     OutputName {
         name: quoted_name.clone(),
         size: quoted_name.len(),
-        quoted: config.quote_name || needs_quote,
+        quoted: needs_quote,
     }
 }
